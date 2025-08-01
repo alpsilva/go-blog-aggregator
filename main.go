838ccf60ebdbc1ce2a052242805635fbaab94da2
André Luiz <alpsilva.dev@gmail.com>
2025-07-31 22:16:15 -0300

    [feat] feed handler

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"errors"
 	"fmt"
+	"html"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
@@ -41,6 +45,22 @@ func (c commands) run(s *state, cmd command) error {
 	}
 
 	return nil
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
 }
 
 func (c commands) register(name string, f func(*state, command) error) {
@@ -136,6 +156,64 @@ func handlerListUsers(s *state, cmd command) error {
 	return nil
 }
 
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		fmt.Println(err)
+		return &RSSFeed{}, err
+	}
+	req.Header.Add("User-Agent", "gator")
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return &RSSFeed{}, err
+	}
+	body := response.Body
+	defer body.Close()
+
+	data, err := io.ReadAll(body)
+	if err != nil {
+		fmt.Println(err)
+		return &RSSFeed{}, err
+	}
+
+	rssFeed := RSSFeed{}
+	err = xml.Unmarshal(data, &rssFeed)
+	if err != nil {
+		fmt.Println(err)
+		return &RSSFeed{}, err
+	}
+
+	return &rssFeed, nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+
+	url := "https://www.wagslane.dev/index.xml"
+
+	ctx := context.Background()
+
+	feed, err := fetchFeed(ctx, url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+	for _, rssItem := range feed.Channel.Item {
+		rssItem.Title = html.UnescapeString(rssItem.Title)
+		rssItem.Description = html.UnescapeString(rssItem.Description)
+	}
+
+	fmt.Println(feed)
+
+	return nil
+}
+
 func main() {
 
 	var err error
@@ -161,6 +239,7 @@ func main() {
 	commandsStc.register("register", handlerRegister)
 	commandsStc.register("users", handlerListUsers)
 	commandsStc.register("reset", handlerReset)
+	commandsStc.register("agg", handlerAgg)
 
 	args := os.Args
 
